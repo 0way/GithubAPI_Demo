305184c8ba9128102efbafcbe7a52dcdd5c70d7b
tranchau1991 <tranchau1991@gmail.com>
2017-04-08 23:09:00 +0700

    Updated file finder.blade.php

--- resources/views/finder.blade.php
+++ resources/views/finder.blade.php
@@ -2,16 +2,17 @@
 
 @section('content')
 <header>
-    <a href="/finder?repo={{ $repo }}&path={{ $parent }}"><h3 class="glyphicon glyphicon-arrow-left"></h3></a>
+    <!--<a href="./finder?repo={{ $repo }}&path={{ $parent }}"><h3 class="glyphicon glyphicon-arrow-left"></h3></a>-->
+    <a href="./finder?repo={{ $repo }}"><h3 class="glyphicon glyphicon-arrow-left"></h3></a>
 </header>
 <ul class="list-group">
     @foreach($items as $item)
         <li class="list-group-item">
             @if(isset($item['type']) && $item['type'] == 'file')
-                <a href="/edit?repo={{ $repo }}&path={{ $item['path'] }}">{{ $item['name'] }}</a>
+                <a href="./edit?repo={{ $repo }}&path={{ $item['path'] }}">{{ $item['name'] }}</a>
                 <span class="badge">F</span>
             @else
-                <a href="/finder?repo={{ $repo }}&path={{ $item['path'] }}">{{ $item['name'] }}</a>
+                <a href="./finder?repo={{ $repo }}&path={{ $item['path'] }}">{{ $item['name'] }}</a>
                 <span class="badge">D</span>
             @endif
         </li>
